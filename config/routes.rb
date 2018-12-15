09ed7e1a005a6de4b8419a33f8b63e1702cb9d4c
rina71 <yurinas@gmail.com>
2018-12-14 23:59:00 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists
   resources :songs
+  resources :artists
+  resources :artists, only: [:index] do
+    resources :songs, only: [:show, :index]
+  end
 end
